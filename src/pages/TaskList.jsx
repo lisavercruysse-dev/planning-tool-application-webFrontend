c9873f6fa432f55ea00741e245b1a233db465326
Mig <migael.delen@student.hogent.be>
2026-02-23 14:52:34 +0100

    Implementatie zoeksysteem

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,10 +1,24 @@
+import { useState, useMemo } from "react";
 import { TASK_DATA } from "../api/mock_data";
-import { TaskCards } from "../components/taskCards";
+import { TaskCards } from "../components/TaskCards";
 
 export default function TaskList() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredTasks = useMemo(() => {
+    if (!searchQuery) return TASK_DATA;
+    return TASK_DATA.filter((task) =>
+      task.omschrijving.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
+  }, [searchQuery]);
+
   return (
-    <div className="mx-16">
-      <TaskCards tasks={TASK_DATA}></TaskCards>
+    <div className="mx-16 mt-8">
+      <TaskCards
+        tasks={filteredTasks}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
     </div>
   );
 }
