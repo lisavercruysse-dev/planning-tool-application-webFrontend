a66d87d78303f1d73e5032a8d94bad4477d3364e
Mig <migael.delen@student.hogent.be>
2026-02-24 14:14:20 +0100

    Implementatie filteren taken op datum

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,19 +1,32 @@
 import { useState, useMemo } from "react";
 import { TASK_DATA } from "../api/mock_data";
 import { TaskCards } from "../components/TaskCards";
+import { DatePicker } from "../components/DatePicker";
 
 export default function TaskList() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
 
   const filteredTasks = useMemo(() => {
-    if (!searchQuery) return TASK_DATA;
-    return TASK_DATA.filter((task) =>
-      task.omschrijving.toLowerCase().includes(searchQuery.toLowerCase()),
-    );
-  }, [searchQuery]);
+    return TASK_DATA.filter((task) => {
+      const matchesSearch = task.omschrijving
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase());
+
+      const taskDate = task.startdatum.split("T")[0];
+      const matchesDate = selectedDate ? taskDate === selectedDate : true;
+
+      return matchesSearch && matchesDate;
+    });
+  }, [searchQuery, selectedDate]);
 
   return (
-    <div className="mx-16 mt-8">
+    <div className="mx-16 mt-8 flex flex-col gap-8">
+      <DatePicker
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+      />
+
       <TaskCards
         tasks={filteredTasks}
         searchQuery={searchQuery}
