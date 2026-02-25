093035c3ceb9fd77a6911cf9ac47179d1f63c2e9
Lisa Vercruysse <lisa.vercruysse@student.hogent.be>
2026-02-25 13:54:52 +0100

    Hover Functionaliteit

--- src/components/menu/MenuItem.jsx
+++ src/components/menu/MenuItem.jsx
@@ -1,6 +1,6 @@
 export default function MenuItem ({name, icon: Icon}) {
     return (
-        <div className="flex gap-3 items-center px-8 py-3">
+        <div className="flex gap-3 items-center px-8 py-3 hover:cursor-pointer hover:text-[#737373]">
             <Icon size='25'/>
             <p>{name}</p>
         </div>
