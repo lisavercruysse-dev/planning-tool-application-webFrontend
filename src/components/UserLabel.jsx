947561418776bd5c7e2085b30b76e43d91b49bed
Lisa Vercruysse <lisa.vercruysse@student.hogent.be>
2026-03-02 14:07:03 +0100

    Code om werknemer op te halen uit db

--- src/components/UserLabel.jsx
+++ src/components/UserLabel.jsx
@@ -1,15 +1,27 @@
 import { FaUserCircle } from "react-icons/fa";
-import { USER_DATA } from "../api/mock_data";
+import useSWR from "swr";
+import AsyncData from "./asyncData/AsyncData";
+import { getById } from "../api";
 
 export default function UserLabel () {
-    const user = USER_DATA[1];
+    const {
+        data: user,
+        isLoading,
+        error,
+    } = useSWR(`/werknemers/${1}`, getById)
 
     return (
         <div className="border border-[#E5E5E5] py-3 flex gap-5 items-center w-60 justify-center rounded-3xl m-4">
             <FaUserCircle className="text-4xl text-[#C4C4C4]"/>
             <div>
-                <p className="font-bold">{user.firstName} {user.lastName}</p>
-                <p className="text-[#4D4D4D]">{user.jobtitel}</p>
+                <AsyncData loading={isLoading} error={error}>
+                    {user && (
+                        <>
+                            <p className="font-bold">{user.firstName} {user.lastName}</p>
+                            <p className="text-[#4D4D4D]">{user.jobTitel}</p>
+                        </>
+                    )}
+                </AsyncData>
             </div>
         </div>
     )
