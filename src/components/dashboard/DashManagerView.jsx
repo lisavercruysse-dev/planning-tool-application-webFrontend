aab9a490f3c5b89eb3ba8b319165317088c18764
PATRICK VAN KERCKHOVE <patrick.vankerckhove@student.hogent.be>
2026-03-20 17:26:45 +0100

    knop details uitgelijnd op dashmanager

--- src/components/dashboard/DashManagerView.jsx
+++ src/components/dashboard/DashManagerView.jsx
@@ -20,22 +20,23 @@ export default function DashManagerView({
         selectedSiteId={selectedSite?.id}
       />
 
-      {/* Onder: placeholder of detail info */}
-      <div className="bg-white rounded-xl border border-gray-200 shadow-sm h-100 flex items-center justify-center">
+      {/* Onder: placeholder knop naar details, infocard en KPI's */}
+      <div className="bg-white rounded-xl border border-gray-200 shadow-sm h-120 flex items-center justify-center">
         {!selectedSite ? (
           <NoSiteSelected />
         ) : (
           <div className="w-full p-6 flex justify-start gap-8">
-            <SiteInfoCard site={selectedSite} userData={userData} />
-
-            {/* Knop naar details */}
-            <div className="flex items-start pt-2">
-              <button
-                onClick={onDetails}
-                className="px-4 py-2 bg-gray-800 hover:bg-gray-700 text-white text-sm font-medium rounded-md transition-colors"
-              >
-                Details
-              </button>
+            <div >
+              {/* Knop naar details */}
+              <div className="flex justify-center mb-4">
+                <button
+                  onClick={onDetails}
+                  className="px-4 py-2 bg-gray-700 hover:bg-gray-500 text-white text-sm font-medium rounded-md transition-colors"
+                >
+                  Details
+                </button>
+              </div>
+              <SiteInfoCard site={selectedSite} userData={userData} />
             </div>
           </div>
         )}
